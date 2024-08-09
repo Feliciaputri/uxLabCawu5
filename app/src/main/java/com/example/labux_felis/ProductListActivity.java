2e7c757ebf48503ca349278ab560454b82a66e99
Jefferson Timotius Mansur <122242388+JeffTimoman@users.noreply.github.com>
2024-08-09 09:00:40 +0700

    omg

--- app/src/main/java/com/example/labux_felis/ProductListActivity.java
+++ app/src/main/java/com/example/labux_felis/ProductListActivity.java
@@ -19,7 +19,11 @@ public class ProductListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
         setContentView(R.layout.activity_product_list);
+
 
         try {
             recyclerView = findViewById(R.id.recyclerView);
